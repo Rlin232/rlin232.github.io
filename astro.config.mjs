690234aca2582bdef31b2082d6a52d2bc8ef3e62
RLin232 <rlin23205@gmail.com>
2025-03-07 01:12:19 -0800

    Integrated google analytics to astro
    Signed-off-by: RLin232 <rlin23205@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   site: 'https://rlin232.github.io',
   integrations: [
     mdx(),
     tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+  }),
   ],
 }); 
