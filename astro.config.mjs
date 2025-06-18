cbf0bb6a8231bc255df52997b02dd96042184a68
RLin232 <rlin23205@gmail.com>
2025-06-18 14:03:17 -0700

    added sitemap
    Signed-off-by: RLin232 <rlin23205@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://rlin232.github.io',
   integrations: [
     mdx(),
+    sitemap(),
     tailwind(),
     partytown({
       config: {
