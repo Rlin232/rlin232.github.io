88fe5cad742d5bd926610403191197fd88f0f227
RLin232 <rlin23205@gmail.com>
2025-10-09 14:54:25 -0700

    New pubs! (Some links not working due to OpenReview not being public yet)
    Signed-off-by: RLin232 <rlin23205@gmail.com>

--- src/data/publications.js
+++ src/data/publications.js
@@ -1,24 +1,84 @@
 export const publications = [
     {
+      title: "CAT: Curvature-Adaptive Transformers for Geometry-Aware Learning",
+      authors: ["Ryan Y. Lin", "Siddhartha Ojha", "Nicholas Bai"],
+      year: "2025",
+      venue: "NeurIPS Workshop on Non-Euclidean Foundation Models and Geometric Learning",
+      links:{
+        pdf: "https://openreview.net/pdf?id=nlmupIN2t7",
+        url: "https://openreview.net/forum?id=nlmupIN2t7"
+      },
+      platform: "OpenReview",
+      preview: "/assets/img/CAT.png",
+      bibtex: `@inproceedings{
+      lin2025cat,
+      title={{CAT}: Curvature-Adaptive Transformers for Geometry-Aware Learning},
+      author={Ryan Y. Lin and Siddhartha Ojha and Nicholas Bai},
+      booktitle={Non-Euclidean Foundation Models: Advancing AI Beyond Euclidean Frameworks},
+      year={2025},
+      url={https://openreview.net/forum?id=nlmupIN2t7}
+      }`
+    },
+    {
+      title: "Coarse-to-Fine 3D MRI Reconstruction via 3D Neural Operators",
+      authors: ["Armeet Singh Jatyani", "Jiayun Wang", "Ryan Y. Lin", "Valentin Duruisseaux", "Anima Anandkumar"],
+      year: "2025",
+      venue: "NeurIPS Workshop on Imageomics: Discovering Biological Knowledge from Images Using AI",
+      links:{
+        pdf: "https://openreview.net/pdf?id=aUyIydXQML",
+        url: "https://openreview.net/forum?id=aUyIydXQML"
+      },
+      platform: "OpenReview",
+      preview: "/assets/img/3DMRI.png",
+      bibtex: `@inproceedings{
+      jatyani2025coarsetofine,
+      title={Coarse-to-Fine 3D {MRI} Reconstruction via 3D Neural Operators},
+      author={Armeet Singh Jatyani and Jiayun Wang and Ryan Y. Lin and Valentin Duruisseaux and Anima Anandkumar},
+      booktitle={NeurIPS 2025 Workshop for Imageomics: Discovering Biological Knowledge from Images Using AI},
+      year={2025},
+      url={https://openreview.net/forum?id=aUyIydXQML}
+      }`
+    },
+    {
+      title: "Diffusion-Generated Social Graphs Enhance Bot Detection",
+      authors: ["Alec Laprevotte", "Ryan Y. Lin", "Siddhartha Ojha"],
+      year: "2025",
+      venue: "NeurIPS Workshop on New Perspectives in Graph Machine Learning",
+      links:{
+        pdf: "https://openreview.net/pdf?id=291xPefJRO",
+        url: "https://openreview.net/forum?id=291xPefJRO"
+      },
+      platform: "OpenReview",
+      preview: "/assets/img/Botnet.png",
+      bibtex: `@inproceedings{
+      laprevotte2025diffusiongenerated,
+      title={Diffusion-Generated Social Graphs Enhance Bot Detection},
+      author={Alec Laprevotte and Ryan Y. Lin and Siddhartha Ojha},
+      booktitle={New Perspectives in Graph Machine Learning},
+      year={2025},
+      url={https://openreview.net/forum?id=291xPefJRO}
+      }`
+    },
+    {
       title: "Enabling Automatic Differentiation with Mollified Graph Neural Operators",
       authors: ["Ryan Y. Lin", "Julius Berner", "Valentin Duruisseaux", "David Pitt", "Daniel Leibovici", "Jean Kossaifi", "Kamyar Azizzadenesheli", "Anima Anandkumar"],
       year: "2025",
-      venue: "arXiv",
+      venue: "Transactions on Machine Learning Research (TMLR)",
       links:{
-        pdf: "https://arxiv.org/pdf/2504.08277",
-        url: "https://arxiv.org/abs/2504.08277"
+        pdf: "https://openreview.net/pdf?id=CGoR1hFAGr",
+        url: "https://openreview.net/forum?id=CGoR1hFAGr"
       },
-      platform: "arXiv",
+      platform: "OpenReview",
       preview: "/assets/img/mGNO.png",
-      bibtex: `@misc{lin2025enablingautomaticdifferentiationmollified,
-  title={Enabling Automatic Differentiation with Mollified Graph Neural Operators}, 
-  author={Ryan Y. Lin and Julius Berner and Valentin Duruisseaux and David Pitt and Daniel Leibovici and Jean Kossaifi and Kamyar Azizzadenesheli and Anima Anandkumar},
-  year={2025},
-  eprint={2504.08277},
-  archivePrefix={arXiv},
-  primaryClass={cs.LG},
-  url={https://arxiv.org/abs/2504.08277}, 
-  }`
+      bibtex: `
+      @article{
+      lin2025enabling,
+      title={Enabling Automatic Differentiation with Mollified Graph Neural Operators},
+      author={Ryan Y. Lin and Julius Berner and Valentin Duruisseaux and David Pitt and Daniel Leibovici and Jean Kossaifi and Kamyar Azizzadenesheli and Anima Anandkumar},
+      journal={Transactions on Machine Learning Research},
+      year={2025},
+      url={https://openreview.net/forum?id=CGoR1hFAGr},
+      }`
     },
     {
       title: "Strategic Collusion of LLM Agents: Market Division in Multi-Commodity Competitions",
@@ -32,13 +92,13 @@ export const publications = [
       platform: "OpenReview",
       preview: "/assets/img/Collusion.png",
       bibtex: `@inproceedings{
-  lin2024strategic,
-  title={Strategic Collusion of {LLM} Agents: Market Division in Multi-Commodity Competitions},
-  author={Ryan Y. Lin and Siddhartha Ojha and Kevin Cai and Maxwell Chen},
-  
-  booktitle={Language Gamification - NeurIPS 2024 Workshop},
-  year={2024},
-  url={https://openreview.net/forum?id=X9vAImw5Yj}
-  }`
+      lin2024strategic,
+      title={Strategic Collusion of {LLM} Agents: Market Division in Multi-Commodity Competitions},
+      author={Ryan Y. Lin and Siddhartha Ojha and Kevin Cai and Maxwell Chen},
+      
+      booktitle={Language Gamification - NeurIPS 2024 Workshop},
+      year={2024},
+      url={https://openreview.net/forum?id=X9vAImw5Yj}
+      }`
     }
   ];
