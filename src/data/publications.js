3dd8f699ad6bc31a6a57ec6f781164483ff42117
RLin232 <rlin23205@gmail.com>
2026-01-08 10:40:33 -0800

    Added Same or Not
    Signed-off-by: RLin232 <rlin23205@gmail.com>

--- src/data/publications.js
+++ src/data/publications.js
@@ -1,16 +1,37 @@
 export const publications = [
-    {
-      title: "CAT: Curvature-Adaptive Transformers for Geometry-Aware Learning",
-      authors: ["Ryan Y. Lin", "Siddhartha Ojha", "Nicholas Bai"],
-      year: "2025",
-      venue: "NeurIPS Workshop on Non-Euclidean Foundation Models and Geometric Learning",
-      links:{
-        pdf: "https://openreview.net/pdf?id=nlmupIN2t7",
-        url: "https://openreview.net/forum?id=nlmupIN2t7"
-      },
-      platform: "OpenReview",
-      preview: "/assets/img/CAT.png",
-      bibtex: `@inproceedings{
+  {
+    title: "Same or Not? Enhancing Visual Perception in Vision-Language Models",
+    authors: ["Damiano Marsili", "Aditya Mehta", "Ryan Y. Lin", "Georgia Gkioxari"],
+    year: "2025",
+    venue: "arXiv Preprint",
+    links: {
+      pdf: "https://arxiv.org/pdf/2512.23592",
+      url: "https://glab-caltech.github.io/twin/"
+    },
+    platform: "arXiv",
+    preview: "/assets/img/twin_logo.png",
+    bibtex: `@misc{marsili2025notenhancingvisualperception,
+      title={Same or Not? Enhancing Visual Perception in Vision-Language Models}, 
+      author={Damiano Marsili and Aditya Mehta and Ryan Y. Lin and Georgia Gkioxari},
+      year={2025},
+      eprint={2512.23592},
+      archivePrefix={arXiv},
+      primaryClass={cs.CV},
+      url={https://arxiv.org/abs/2512.23592}, 
+      }`
+  },
+  {
+    title: "CAT: Curvature-Adaptive Transformers for Geometry-Aware Learning",
+    authors: ["Ryan Y. Lin", "Siddhartha Ojha", "Nicholas Bai"],
+    year: "2025",
+    venue: "NeurIPS Workshop on Non-Euclidean Foundation Models and Geometric Learning",
+    links: {
+      pdf: "https://openreview.net/pdf?id=nlmupIN2t7",
+      url: "https://openreview.net/forum?id=nlmupIN2t7"
+    },
+    platform: "OpenReview",
+    preview: "/assets/img/CAT.png",
+    bibtex: `@inproceedings{
       lin2025cat,
       title={{CAT}: Curvature-Adaptive Transformers for Geometry-Aware Learning},
       author={Ryan Y. Lin and Siddhartha Ojha and Nicholas Bai},
@@ -18,19 +39,19 @@ export const publications = [
       year={2025},
       url={https://openreview.net/forum?id=nlmupIN2t7}
       }`
+  },
+  {
+    title: "Coarse-to-Fine 3D MRI Reconstruction via 3D Neural Operators",
+    authors: ["Armeet Singh Jatyani", "Jiayun Wang", "Ryan Y. Lin", "Valentin Duruisseaux", "Anima Anandkumar"],
+    year: "2025",
+    venue: "NeurIPS Workshop on Imageomics: Discovering Biological Knowledge from Images Using AI",
+    links: {
+      pdf: "https://openreview.net/pdf?id=aUyIydXQML",
+      url: "https://openreview.net/forum?id=aUyIydXQML"
     },
-    {
-      title: "Coarse-to-Fine 3D MRI Reconstruction via 3D Neural Operators",
-      authors: ["Armeet Singh Jatyani", "Jiayun Wang", "Ryan Y. Lin", "Valentin Duruisseaux", "Anima Anandkumar"],
-      year: "2025",
-      venue: "NeurIPS Workshop on Imageomics: Discovering Biological Knowledge from Images Using AI",
-      links:{
-        pdf: "https://openreview.net/pdf?id=aUyIydXQML",
-        url: "https://openreview.net/forum?id=aUyIydXQML"
-      },
-      platform: "OpenReview",
-      preview: "/assets/img/3DMRI.png",
-      bibtex: `@inproceedings{
+    platform: "OpenReview",
+    preview: "/assets/img/3DMRI.png",
+    bibtex: `@inproceedings{
       jatyani2025coarsetofine,
       title={Coarse-to-Fine 3D {MRI} Reconstruction via 3D Neural Operators},
       author={Armeet Singh Jatyani and Jiayun Wang and Ryan Y. Lin and Valentin Duruisseaux and Anima Anandkumar},
@@ -38,19 +59,19 @@ export const publications = [
       year={2025},
       url={https://openreview.net/forum?id=aUyIydXQML}
       }`
+  },
+  {
+    title: "Diffusion-Generated Social Graphs Enhance Bot Detection",
+    authors: ["Alec Laprevotte", "Ryan Y. Lin", "Siddhartha Ojha"],
+    year: "2025",
+    venue: "NeurIPS Workshop on New Perspectives in Graph Machine Learning",
+    links: {
+      pdf: "https://openreview.net/pdf?id=291xPefJRO",
+      url: "https://openreview.net/forum?id=291xPefJRO"
     },
-    {
-      title: "Diffusion-Generated Social Graphs Enhance Bot Detection",
-      authors: ["Alec Laprevotte", "Ryan Y. Lin", "Siddhartha Ojha"],
-      year: "2025",
-      venue: "NeurIPS Workshop on New Perspectives in Graph Machine Learning",
-      links:{
-        pdf: "https://openreview.net/pdf?id=291xPefJRO",
-        url: "https://openreview.net/forum?id=291xPefJRO"
-      },
-      platform: "OpenReview",
-      preview: "/assets/img/Botnet.png",
-      bibtex: `@inproceedings{
+    platform: "OpenReview",
+    preview: "/assets/img/Botnet.png",
+    bibtex: `@inproceedings{
       laprevotte2025diffusiongenerated,
       title={Diffusion-Generated Social Graphs Enhance Bot Detection},
       author={Alec Laprevotte and Ryan Y. Lin and Siddhartha Ojha},
@@ -58,19 +79,19 @@ export const publications = [
       year={2025},
       url={https://openreview.net/forum?id=291xPefJRO}
       }`
+  },
+  {
+    title: "Enabling Automatic Differentiation with Mollified Graph Neural Operators",
+    authors: ["Ryan Y. Lin", "Julius Berner", "Valentin Duruisseaux", "David Pitt", "Daniel Leibovici", "Jean Kossaifi", "Kamyar Azizzadenesheli", "Anima Anandkumar"],
+    year: "2025",
+    venue: "Transactions on Machine Learning Research (TMLR)",
+    links: {
+      pdf: "https://openreview.net/pdf?id=CGoR1hFAGr",
+      url: "https://openreview.net/forum?id=CGoR1hFAGr"
     },
-    {
-      title: "Enabling Automatic Differentiation with Mollified Graph Neural Operators",
-      authors: ["Ryan Y. Lin", "Julius Berner", "Valentin Duruisseaux", "David Pitt", "Daniel Leibovici", "Jean Kossaifi", "Kamyar Azizzadenesheli", "Anima Anandkumar"],
-      year: "2025",
-      venue: "Transactions on Machine Learning Research (TMLR)",
-      links:{
-        pdf: "https://openreview.net/pdf?id=CGoR1hFAGr",
-        url: "https://openreview.net/forum?id=CGoR1hFAGr"
-      },
-      platform: "OpenReview",
-      preview: "/assets/img/mGNO.png",
-      bibtex: `
+    platform: "OpenReview",
+    preview: "/assets/img/mGNO.png",
+    bibtex: `
       @article{
       lin2025enabling,
       title={Enabling Automatic Differentiation with Mollified Graph Neural Operators},
@@ -79,19 +100,19 @@ export const publications = [
       year={2025},
       url={https://openreview.net/forum?id=CGoR1hFAGr},
       }`
+  },
+  {
+    title: "Strategic Collusion of LLM Agents: Market Division in Multi-Commodity Competitions",
+    authors: ["Ryan Y. Lin", "Siddhartha Ojha", "Kevin Cai", "Maxwell F. Chen"],
+    year: "2024",
+    venue: "NeurIPS Workshop on Language Gamification",
+    links: {
+      pdf: "https://openreview.net/pdf?id=X9vAImw5Yj",
+      url: "https://openreview.net/forum?id=X9vAImw5Yj"
     },
-    {
-      title: "Strategic Collusion of LLM Agents: Market Division in Multi-Commodity Competitions",
-      authors: ["Ryan Y. Lin", "Siddhartha Ojha", "Kevin Cai", "Maxwell F. Chen"],
-      year: "2024",
-      venue: "NeurIPS Workshop on Language Gamification",
-      links: {
-        pdf: "https://openreview.net/pdf?id=X9vAImw5Yj",
-        url: "https://openreview.net/forum?id=X9vAImw5Yj"
-      },
-      platform: "OpenReview",
-      preview: "/assets/img/Collusion.png",
-      bibtex: `@inproceedings{
+    platform: "OpenReview",
+    preview: "/assets/img/Collusion.png",
+    bibtex: `@inproceedings{
       lin2024strategic,
       title={Strategic Collusion of {LLM} Agents: Market Division in Multi-Commodity Competitions},
       author={Ryan Y. Lin and Siddhartha Ojha and Kevin Cai and Maxwell Chen},
@@ -100,5 +121,5 @@ export const publications = [
       year={2024},
       url={https://openreview.net/forum?id=X9vAImw5Yj}
       }`
-    }
-  ];
+  }
+];
